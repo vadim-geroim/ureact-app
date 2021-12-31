20852eb0ed7de6d8ec823e6f15aec957234b7e0f
vgeroim <vgeroim.qa@gmail.com>
2021-12-30 18:29:16 -0600

    Create basic project structure

--- src/index.js
+++ src/index.js
@@ -2,12 +2,14 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import { Header } from './App';
 import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
   <React.StrictMode>
     <App />
-  </React.StrictMode>,
+  </React.StrictMode>
+  ,
   document.getElementById('root')
 );
 
